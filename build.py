7d6196f01b8607e37960bdf31b97f88763cab748
Vaibhav <vrongmeal@gmail.com>
2020-02-07 12:35:55 +0530

    refactor: Cleanup `build.py`. (#3)
    Signed-off-by: Vaibhav <vrongmeal@gmail.com>

--- build.py
+++ build.py
@@ -1,34 +1,51 @@
+"""
+This along-with gh-pages hosts a static website which watchdog uses to check
+if the person has access to ssh on the machine running watchdog.
+"""
+
 import os
-import sys
 import hashlib
-keys = {}
-file = open("data/keys")
-lines = file.readlines()
-os.makedirs("api/names/")
-for l in lines:
-	idx = l.index('|')
-	sshkey = l[idx+1:len(l)-1]
-	sshkey = sshkey.split(' ')[0] + " " + sshkey.split(' ')[1]
-	keys[l[:idx]] = hashlib.sha256(sshkey.encode()).hexdigest()
-	f = open("api/names/{}".format(keys[l[:idx]]), "w")
-	f.write(l[:idx])
-	f.close()
+
+
+API_ACCESS = "api/access"
+API_NAMES = "api/names"
+DATA_KEYS = "data/keys"
+DATA_HOSTS = "data/hosts"
+
 
 def make_route(host, ruser, keyhash):
-	if os.path.exists("api/access/{}/{}".format( host, ruser)) == False:
-		os.makedirs("api/access/{}/{}".format(host, ruser))
-	f = open("api/access/{}/{}/{}".format(host, ruser, keyhash), "w")
-	f.write("1")
-	f.close()
+    if os.path.exists("{}/{}/{}".format(API_ACCESS, host, ruser)) is False:
+        os.makedirs("{}/{}/{}".format(API_ACCESS, host, ruser))
+    f = open("{}/{}/{}/{}".format(API_ACCESS, host, ruser, keyhash), "w")
+    f.write("1")
+    f.close()
 
-for hostname in os.listdir("data/hosts"):
-	fp = open("data/hosts/{}".format(hostname))
-	lines = fp.readlines()
-	print(hostname)
-	for l in lines:
-		if l == '\n':
-			continue
-		idx = l.index('|')
-		ruser = l[idx+1:len(l)-1]
-		keyhash = keys[l[:idx]]
-		make_route(hostname, ruser, keyhash)
+
+keys = {}
+
+file = open(DATA_KEYS)
+lines = file.readlines()
+file.close()
+
+os.makedirs("{}/".format(API_NAMES))
+
+for line in lines:
+    idx = line.index("|")
+    sshkey = line[idx + 1: len(line) - 1]
+    sshkey = "{} {}".format(sshkey.split(" ")[0], sshkey.split(" ")[1])
+    keys[line[:idx]] = hashlib.sha256(sshkey.encode()).hexdigest()
+    f = open("{}/{}".format(API_NAMES, keys[line[:idx]]), "w")
+    f.write(line[:idx])
+    f.close()
+
+for hostname in os.listdir(DATA_HOSTS):
+    fp = open("{}/{}".format(DATA_HOSTS, hostname))
+    lines = fp.readlines()
+    fp.close()
+    for line in lines:
+        if line == "\n":
+            continue
+        idx = line.index("|")
+        ruser = line[idx + 1: len(line) - 1]
+        keyhash = keys[line[:idx]]
+        make_route(hostname, ruser, keyhash)
